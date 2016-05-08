9f78356dde3475ad4206c2e2ba924eb22a2462fc
Henrique Moody <henriquemoody@gmail.com>
2016-05-08 12:01:28 +0200

    Create new interface for rules

---
+++ library/Rules/RuleInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+namespace Respect\Validation\Rules;
+
+use Respect\Validation\Result;
+
+/**
+ * Default interface for rules.
+ *
+ * @author Henrique Moody <henriquemoody@gmail.com>
+ */
+interface RuleInterface
+{
+    /**
+     * Validates the rule against the given input.
+     *
+     * @param mixed $input
+     *
+     * @return Result
+     */
+    public function validate($input);
+}
