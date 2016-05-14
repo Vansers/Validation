4b6281fc9c1d0ccd5e0276775996e43e0cd87a8b
Henrique Moody <henriquemoody@gmail.com>
2016-05-15 00:03:40 +0200

    Create abstract class `AbstractComposite2`
    This class must be used by nested rules, like `allOf()`, `oneOf()` and
    `noneOf()`.
    
    It could just be a refactoring of `AbstractComposer` but it would cause
    too may errors and the commit would be enormous. It might be renamed to
    `AbstractComposite` in the future, thought.

---
+++ library/Rules/AbstractComposite2.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+namespace Respect\Validation\Rules;
+
+/**
+ * Abstract class to handle nested rules.
+ */
+abstract class AbstractComposite2 implements RuleInterface
+{
+    /**
+     * @var RuleInterface[]
+     */
+    private $rules = [];
+
+    /**
+     * @param RuleInterface ...$rules
+     */
+    public function __construct(RuleInterface ...$rules)
+    {
+        $this->addRules($rules);
+    }
+
+    /**
+     * @return RuleInterface[]
+     */
+    public function getRules()
+    {
+        return $this->rules;
+    }
+
+    /**
+     * @param RuleInterface $rule
+     */
+    public function addRule(RuleInterface $rule)
+    {
+        $this->rules[] = $rule;
+    }
+
+    /**
+     * @param RuleInterface[] $rules
+     */
+    public function addRules(array $rules)
+    {
+        foreach ($rules as $rule) {
+            $this->addRule($rule);
+        }
+
+        return $this;
+    }
+}
---
+++ tests/unit/Rules/AbstractComposite2Test.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+namespace Respect\Validation\Rules;
+
+/**
+ * @covers Respect\Validation\Rules\AbstractComposite2
+ */
+final class AbstractComposite2Test extends \PHPUnit_Framework_TestCase
+{
+    private function getAbstractComposite2MockWithoutConstructor()
+    {
+        return $this
+            ->getMockBuilder(AbstractComposite2::class)
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+    }
+
+    public function getRuleMocksByQuantity($quantity)
+    {
+        $ruleMocks = [];
+        for ($index = 0; $index < $quantity; ++$index) {
+            $ruleMocks[] = $this->createMock(RuleInterface::class);
+        }
+
+        return $ruleMocks;
+    }
+
+    public function testShouldHaveNoNestedRulesByDefault()
+    {
+        $abstractNestedMock = $this->getAbstractComposite2MockWithoutConstructor();
+
+        $this->assertEmpty($abstractNestedMock->getRules());
+    }
+
+    public function testShouldBeAbleToAddARule()
+    {
+        $ruleMock = $this->createMock(RuleInterface::class);
+
+        $abstractNestedMock = $this->getAbstractComposite2MockWithoutConstructor();
+        $abstractNestedMock->addRule($ruleMock);
+
+        $expectedRules = [$ruleMock];
+
+        $this->assertSame($expectedRules, $abstractNestedMock->getRules());
+    }
+
+    public function testShouldBeAbleToAddRules()
+    {
+        $rules = $this->getRuleMocksByQuantity(3);
+
+        $abstractNestedMock = $this->getAbstractComposite2MockWithoutConstructor();
+        $abstractNestedMock->addRules($rules);
+
+        $this->assertSame($rules, $abstractNestedMock->getRules());
+    }
+
+    public function testShouldNotOverwriteWhenAddingRules()
+    {
+        $rules = $this->getRuleMocksByQuantity(4);
+        $rule1 = array_shift($rules);
+        list($rule2, $rule3, $rule4) = $rules;
+
+        $abstractNestedMock = $this->getAbstractComposite2MockWithoutConstructor();
+        $abstractNestedMock->addRule($rule1);
+        $abstractNestedMock->addRules($rules);
+
+        $expectedRules = [$rule1, $rule2, $rule3, $rule4];
+
+        $this->assertSame($expectedRules, $abstractNestedMock->getRules());
+    }
+
+    public function testShouldBeAbleToAddRulesOnConstructor()
+    {
+        $rules = $this->getRuleMocksByQuantity(3);
+
+        $abstractNestedMock = $this
+            ->getMockBuilder(AbstractComposite2::class)
+            ->setConstructorArgs($rules)
+            ->getMockForAbstractClass();
+
+        $this->assertSame($rules, $abstractNestedMock->getRules());
+    }
+}
